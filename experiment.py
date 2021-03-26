3e170d3362ac96bff61fcd891d7524da8a93ffee
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 16:00:55 +0100

    Start experiment definition

---
+++ experiment.py
@@ -0,0 +1,84 @@
+"""Atomic unit of experiment is one full train run on one fold
+   Experiment will run only on machine with >= 1 GPU
+"""
+import argparse
+import logging
+import os
+
+import torch
+import pandas as pd
+
+from shopee.config import Config, save_config_yaml
+from shopee.log_utils import get_commit_hash, get_exp_name, setup_logg
+from shopee.paths import MODELS_PATH, LOGS_PATH, META_PATH, DATA_ROOT
+
+parser = argparse.ArgumentParser(description="Experiment")
+parser.add_argument(
+    "--fold",
+    "-F",
+    type=int,
+    default=0,
+    choices=[0, 1, 2, 3],
+    help="Fold # to use for validation. Default=0.",
+)
+parser.add_argument(
+    "--train_csv",
+    default="train_folds.csv",
+    help="CSV file to use in training. Make sure it conforms to default file",
+)
+parser.add_argument(
+    "--resume",
+    default="",
+    type=str,
+    help="Resume full model and optimizer state from checkpoint (default: none)",
+)
+# this arg is required to utilize DDP training
+parser.add_argument("--local_rank", default=0, type=int)
+
+
+def main():
+    exp_name = get_exp_name(debug=Config["debug"])
+    args = parser.parse_args()
+
+    checkpoint_path = None
+    # Resume Experiment is provided
+    if args.resume:
+        checkpoint_path = MODELS_PATH / f"{args.resume}_f{args.fold}.pth"
+        assert (
+            checkpoint_path.exists()
+        ), f"Experiment {args.resume} checkpoint doesn't exist"
+        exp_name = args.resume
+
+    # Save training confiig
+    save_config_yaml(Config, LOGS_PATH, exp_name)
+    # Setup logging
+    _ = setup_logg(LOGS_PATH, exp_name)
+    logging.info(f"Experiment: {exp_name}")
+
+    # Setup distributed training if avail
+    args.distributed = False
+    if "WORLD_SIZE" in os.environ:
+        args.distributed = int(os.environ["WORLD_SIZE"]) > 1
+    args.device = "cuda:0"
+    args.world_size = 1
+    args.rank = 0  # global rank
+    if args.distributed:
+        args.device = "cuda:%d" % args.local_rank
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(backend="nccl", init_method="env://")
+        args.world_size = torch.distributed.get_world_size()
+        args.rank = torch.distributed.get_rank()
+        logging.info(
+            "Training in distributed mode with multiple processes, 1 GPU per process. Process %d, total %d."
+            % (args.rank, args.world_size)
+        )
+    else:
+        logging.info("Training with a single process on 1 GPUs.")
+    assert args.rank >= 0
+    # seed
+    torch.manual_seed(Config["seed"] + args.rank)
+
+    logging.info(f"Git commit hash: {get_commit_hash()}")
+    logging.info(f"Using CSV: {META_PATH / args.train_csv}")
+
+    full_df = pd.read_csv(META_PATH / args.train_csv)
