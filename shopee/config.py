63257714ff56a5640d8085b7a32d1056005cc19b
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 12:17:21 +0100

    Add model specific fields

--- shopee/config.py
+++ shopee/config.py
@@ -1,6 +1,14 @@
 Config = {
+    # data
     "image_id_col": "image",
     "target_col": "target",
     "img_size": 384,  # resize
     "crop_size": None,  # if none == img_size
+    # model bacbone
+    "arch": "resnet50d",
+    "pretrained": True,
+    "global_pool": "avg",
+    "drop_rate": 0.0,
+    "model_kwargs": {"drop_path_rate": None},
+    "bn_momentum": 0.1,  # default 0.1
 }
