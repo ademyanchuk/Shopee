297060ee852ef13c3605526ef16ed10d985b3490
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 13:50:25 +0100

    Add arcface model

--- shopee/models.py
+++ shopee/models.py
@@ -1,7 +1,7 @@
+import timm
 import torch
 from torch import nn
 from torch.nn import functional as F
-import timm
 
 
 def timm_backbone(
@@ -32,3 +32,39 @@ class ArcFaceLayer(nn.Module):
         x_norm = F.normalize(x)
         W_norm = F.normalize(self.W, dim=0)
         return x_norm @ W_norm
+
+
+class ArcFaceNet(nn.Module):
+    def __init__(
+        self, num_classes: int, Config: dict,
+    ):
+        super(ArcFaceNet, self).__init__()
+        self.backbone = timm_backbone(
+            f_out=0,
+            arch=Config["arch"],
+            pretrained=False,
+            global_pool=Config["global_pool"],
+            drop_rate=Config["drop_rate"],
+            bn_momentum=Config["bn_momentum"],
+            **Config["model_kwargs"],
+        )
+        num_features = self.backbone.num_features
+
+        self.bn1 = nn.BatchNorm2d(num_features)
+        self.dropout = nn.Dropout2d(Config["drop_rate"], inplace=True)
+        self.fc1 = nn.Linear(num_features, Config["embed_size"])
+        self.bn2 = nn.BatchNorm1d(Config["embed_size"])
+
+        self.margin = ArcFaceLayer(
+            emb_size=Config["embed_size"], output_classes=num_classes
+        )
+
+    def forward(self, x, labels=None):
+        features = self.backbone(x)
+        features = self.bn1(features)
+        features = self.dropout(features)
+        features = self.fc1(features)
+        features = self.bn2(features)
+        if labels is not None:
+            return self.margin(features, labels)
+        return F.normalize(features)
