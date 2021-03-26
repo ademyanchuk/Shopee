c1e43442a9b0073f6d6f50cbaa1a88c3a3a29ccd
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 16:00:35 +0100

    Update config

--- shopee/config.py
+++ shopee/config.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+
+import yaml
+
 Config = {
+    "debug": True,
+    "seed": 42,
     # data
     "image_id_col": "image",
     "target_col": "target",
@@ -12,4 +18,12 @@ Config = {
     "drop_rate": 0.0,
     "model_kwargs": {"drop_path_rate": None},
     "bn_momentum": 0.1,  # default 0.1
+    # loss
+    "s": 10,  # arcface s scalar
+    "m": 0.5,  # arcface margin
 }
+
+
+def save_config_yaml(config: dict, log_dir: Path, exp_name: str):
+    with open(log_dir / f"{exp_name}_conf.yaml", "w") as outfile:
+        yaml.dump(config, outfile, default_flow_style=False, sort_keys=False)
