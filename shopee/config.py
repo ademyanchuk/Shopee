80f1c30c5f47e880b77de01cadb13b9cbf18beaf
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 13:49:51 +0100

    Update config

--- shopee/config.py
+++ shopee/config.py
@@ -8,6 +8,7 @@ Config = {
     "arch": "resnet50d",
     "pretrained": True,
     "global_pool": "avg",
+    "embed_size": 512,
     "drop_rate": 0.0,
     "model_kwargs": {"drop_path_rate": None},
     "bn_momentum": 0.1,  # default 0.1
