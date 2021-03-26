e2f5c7091645ac8288d99e6523bac46a53f7de4c
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 12:17:32 +0100

    Add backbone function

--- shopee/models.py
+++ shopee/models.py
@@ -1,6 +1,25 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+import timm
+
+
+def timm_backbone(
+    f_out: int,
+    arch: str,
+    pretrained: bool,
+    global_pool: str,
+    drop_rate: float,
+    bn_momentum: float,
+    **kwargs,
+) -> nn.Module:
+    m = timm.create_model(arch, pretrained=pretrained, drop_rate=drop_rate, **kwargs)
+    m.reset_classifier(f_out, global_pool=global_pool)
+    # reset batch_norm momentum
+    for module in m.modules():
+        if isinstance(module, nn.BatchNorm2d):
+            module.momentum = bn_momentum
+    return m
 
 
 class ArcFaceLayer(nn.Module):
