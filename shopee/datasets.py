bd958eea54f086c4f0c1091c80f4d24da0bc1671
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-03-26 11:03:22 +0100

    Add image dataset

---
+++ shopee/datasets.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+
+
+class ShImageDataset(Dataset):
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        image_dir: Path,
+        image_id_col: str,
+        target_col: str,
+        is_test: bool,
+        transform=None,
+    ):
+        self.df = df
+        self.image_dir = image_dir
+        self.image_id_col = image_id_col
+        self.labels = df[target_col].values
+        self.is_test = is_test
+        self.transform = transform
+
+    def _get_img_path(self, idx: int) -> Path:
+        image_id = self.df.loc[idx, self.image_id_col]
+        file_path = self.image_dir / f"{image_id}"
+        return file_path
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx: int):
+        data = {}
+
+        file_path = self._get_img_path(idx)
+
+        # here input will be greyscale
+        image = Image.open(file_path).convert("RGB")
+
+        if self.transform:
+            image = np.array(image)  # albumentations need numpy
+            image = self.transform(image=image)["image"]
+
+        data["image"] = image
+        if self.is_test:
+            return data
+
+        # train mode
+        label = torch.tensor(self.labels[idx]).long()
+        data["label"] = label
+
+        return data
