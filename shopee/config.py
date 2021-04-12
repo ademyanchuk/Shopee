fac808691b0194321e79e53aa5704d4046c4f57f
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-04-12 09:42:03 +0200

    next exp

--- shopee/config.py
+++ shopee/config.py
@@ -10,14 +10,14 @@ Config = {
     "target_col": "target",
     "img_size": 512,  # resize
     "crop_size": None,  # if none == img_size
-    "bs": 48,
+    "bs": 32,
     "num_workers": 4,
     # model
-    "arch": "tf_efficientnet_b0_ns",
+    "arch": "tf_efficientnet_b1_ns",
     "pretrained": True,
     "global_pool": "catavgmax",
-    "embed_size": 256,
-    "drop_rate": 0.0,
+    "embed_size": 512,
+    "drop_rate": 0.1,
     "model_kwargs": {"drop_path_rate": None},
     "bn_momentum": 0.1,  # default 0.1
     "channels_last": False,
@@ -26,23 +26,23 @@ Config = {
     "model_ema_decay": 0.999,
     "model_ema_force_cpu": False,
     # optimizer
-    "opt_conf": {"adam": {"lr": 1e-3, "weight_decay": 0.0}},
+    "opt_conf": {"adam": {"lr": 3e-4, "weight_decay": 0.0}},
     "sch_conf": {
         "cosine": {
-            "t_initial": 20,
-            "lr_min": 1e-7,
-            "warmup_t": 2,
-            "warmup_lr_init": 5e-6,
+            "t_initial": 15,
+            "lr_min": 1e-6,
+            "warmup_t": 5,
+            "warmup_lr_init": 1e-5,
         }
     },
     # loss
     "s": 30,  # arcface s scalar
-    "m": 0.7,  # arcface margin
+    "m": 0.5,  # arcface margin
     # train
     "aug_type": "albu",
     "rand_aug_severity": 5,
     "rand_aug_width": 5,
-    "num_epochs": 20,
+    "num_epochs": 15,
     "return_best": "score",
     "accum_grad": 1,
     "clip_grad": 1.0,  # norm of parameters grad
