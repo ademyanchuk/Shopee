447cbc6899a5a4bc18244083b4a0566236eb29ac
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-04-27 11:39:52 +0200

    Exp diff lr

--- shopee/config.py
+++ shopee/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import yaml
 
 Config = {
-    "debug": True,
+    "debug": False,
     "seed": 42,
     # data
     "image_id_col": "image",
@@ -32,10 +32,10 @@ Config = {
     "opt_conf": {"adam": {"lr": 5e-4, "weight_decay": 0.0}},
     "sch_conf": {
         "cosine": {
-            "t_initial": 4,
+            "t_initial": 10,
             "lr_min": 1e-6,
-            "warmup_t": 2,
-            "warmup_lr_init": 1e-6,
+            "warmup_t": 3,
+            "warmup_lr_init": 1e-5,
         }
     },
     # loss
@@ -45,7 +45,7 @@ Config = {
     "aug_type": "albu",
     "rand_aug_severity": 5,
     "rand_aug_width": 5,
-    "num_epochs": 4,
+    "num_epochs": 10,
     "return_best": "score",
     "accum_grad": 1,
     "clip_grad": 1.0,  # norm of parameters grad
