351b0635d0a48c9bd6d28050edc068c5d8487f38
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-05-07 09:39:10 +0200

    Add less restrictive threshold coeffs

--- shopee/predict_test.py
+++ shopee/predict_test.py
@@ -126,7 +126,7 @@ def predict_img_text(
         df,
         chunk_sz=1024,
         static_th=static_ths[0],
-        coeff=torch.tensor([0.99, 0.95, 0.9]),
+        coeff=torch.tensor([0.9, 0.85, 0.8]),
     )
 
     # texts
@@ -144,7 +144,7 @@ def predict_img_text(
         df,
         chunk_sz=1024,
         static_th=static_ths[1],
-        coeff=torch.tensor([0.99, 0.95, 0.9]),
+        coeff=torch.tensor([0.9, 0.85, 0.8]),
     )
 
     tmp_df = pd.DataFrame({"img_matches": img_matches, "text_matches": text_matches})
