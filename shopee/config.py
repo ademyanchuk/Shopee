d3be1e5e71a108ef7a5a957f8dd4025b54094917
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-05-14 11:10:43 +0200

    smaller net

--- shopee/config.py
+++ shopee/config.py
@@ -16,7 +16,7 @@ Config = {
     # model
     "bert_name": "sentence-transformers/bert-base-nli-mean-tokens",
     "arc_face_text": False,
-    "arch": "eca_nfnet_l1",
+    "arch": "eca_nfnet_l0",
     "pretrained": True,
     "global_pool": "catavgmax",
     "embed_size": 1024,
